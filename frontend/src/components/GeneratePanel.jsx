46a306ac8ef3d7354453d34ef88c29a75010ffc9
Mithlesh Rana <techmr236@gmail.com>
2026-04-24 14:49:35 +0530

    Fix import case for WaveformBars component

--- frontend/src/components/GeneratePanel.jsx
+++ frontend/src/components/GeneratePanel.jsx
@@ -1,7 +1,7 @@
 ﻿import React, { useState } from 'react';
 import { Sparkles, AlertCircle, ChevronDown, ChevronUp } from 'lucide-react';
 import AudioPlayer from './AudioPlayer';
-import WaveformBars from './WaveformBars';
+import WaveformBars from './waveformBars';
 import { generateVoice } from '../hooks/useApi';
 import { toast } from './Toast';
 
